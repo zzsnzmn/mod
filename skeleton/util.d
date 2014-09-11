ca2b213cfe17f01aab0351a99569bba8974f51ad
brian crabtree <tehn@monome.org>
2014-09-11 09:58:10 -0400

    remove .d

--- skeleton/util.d
+++
@@ -1,9 +0,0 @@
-../skeleton/util.o: ../skeleton/util.c ../skeleton/types.h \
- /Users/tehn/avr32-tools-20140411/lib/gcc/avr32/4.4.7/../../../../avr32/include/stdint.h \
- ../skeleton/util.h
-
-../skeleton/types.h:
-
-/Users/tehn/avr32-tools-20140411/lib/gcc/avr32/4.4.7/../../../../avr32/include/stdint.h:
-
-../skeleton/util.h:
