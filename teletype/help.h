d0488e4c46066e143c0478b5c1f4d283e845df85
brian crabtree <tehn@monome.org>
2015-06-10 14:34:37 -0400

    help

---
+++ teletype/help.h
@@ -0,0 +1,132 @@
+#include "teletype.h"
+
+#define HELP1_LENGTH  36
+ const char * help1[HELP1_LENGTH] = {
+	WELCOME,
+	" ",
+	"TAB`EDIT/LIVE",
+	"TILDE|TRACKER",
+	"ESC|SCENE",
+	"ALT-ESC|WRITE",
+	" ",
+	"ALT-X|CUT",
+	"ALT-C|COPY",
+	"ALT-V|PASTE",
+	" ",
+	"// LIVE",
+	"ENTER|EXECUTE",
+	"UP|PREVIOUS",
+	"SH-BSP|CLEAR",
+	" ",
+	"// EDIT",
+	"[,]|PREV, NEXT SCRIPT",
+	"ENTER|ADD/OVERWRITE",
+	"SH-ENTER|INSERT",
+	"SH-BSP|CLEAR",
+	" ",
+	"// TRACKER",
+	"ARROWS|NAVIGATE",
+	"ALT-ARROWS|JUMP",
+	"0-9|NUMERIC ENTRY",
+	"-|FLIP SIGN",
+	"SPACE|TOGGLE 0/1",
+	"[,]|NUDGE UP, DOWN",
+	"SH-ALT-V|INSERT PASTE",
+	"SH-BSP|DELETE",
+	"SH-ENTER|DUPE INSERT",
+	"SH-L|SET LENGTH",
+	"SH-S|SET START",
+	"SH-E|SET END",
+	"ALT-L,S,E|JUMP"
+};
+
+#define HELP2_LENGTH  9
+ const char * help2[HELP2_LENGTH] = {
+	"PAGE 2",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP3_LENGTH  9
+ const char * help3[HELP3_LENGTH] = {
+	"PAGE 3",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP4_LENGTH  9
+ const char * help4[HELP4_LENGTH] = {
+	"PAGE 4",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP5_LENGTH  9
+ const char * help5[HELP5_LENGTH] = {
+	"PAGE 5",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP6_LENGTH  9
+ const char * help6[HELP6_LENGTH] = {
+	"PAGE 6",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP7_LENGTH  9
+ const char * help7[HELP7_LENGTH] = {
+	"PAGE 7",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
+
+#define HELP8_LENGTH  9
+ const char * help8[HELP8_LENGTH] = {
+	"PAGE 8",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
